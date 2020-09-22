93bf0a3b4c872ce0a48fd3ddd1f9e6df9f1466f6
vennila29 <epsivennila@gmail.com>
2020-09-22 10:20:27 +0530

    third commit-changes on auto update

--- main.js
+++ main.js
@@ -1,68 +1,71 @@
-const {app,window, BrowserWindow, ipcMain} = require('electron')
-const path = require('path') 
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater")
 
-let mainWindow
+let win
 
-
-
-function createWindow () {
-  // Create the browser window.
-// var subpy = require('child_process').spawn('dist/proyecto_ejemplo/proyecto_ejemplo.exe', ['runserver']);
-  // var subpy = require('child_process').spawn( 'python', [__dirname+'/mysite/'+'manage.py', 'runserver']);
-
-    // var rq = require('request-promise');
-    var mainAddr = 'http://localhost:8000/home/';
-    var openWindow = function() {
-        mainWindow = new BrowserWindow({ width: 1200, height: 600 })
-        mainWindow.setMenu(null)
-        mainWindow.loadURL('http://localhost:8000/polls/');
-
-        mainWindow.on('closed', function() {
-            mainWindow = null;
-            console.log("closing")
-            // subpy.kill('SIGINT');
-        })
-    }
-    openWindow();
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  //check for auto update
-  mainWindow.once('ready-to-show', () => {
-      autoUpdater.checkForUpdatesAndNotify();
-  });
+const dispatch = (data) => {
+  win.webContents.send('message', data)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+const createDefaultWindow = () => {
+  win = new BrowserWindow({webPreferences: {
+    nodeIntegration: true
+    }})
+
+  win.on('closed', () => {
+    win = null
+  })
+
+  win.loadFile('index.html')
+
+  return win
+}
+
+app.on('ready', () => {
+  
+  createDefaultWindow()
+  // win.webContents.openDevTools()
+  // win.webContents.send('version', app.getVersion())
+
+  autoUpdater.checkForUpdatesAndNotify()
+
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('version', app.getVersion())
+  })
+
+})
 
 app.on('window-all-closed', () => {
-
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-  
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+  app.quit()
 })
 
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
-});
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
-});
+autoUpdater.on('checking-for-update', () => {
+  dispatch('Checking for update...')
+})
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-  
+autoUpdater.on('update-available', (info) => {
+  dispatch('Update available.')
+})
+
+autoUpdater.on('update-not-available', (info) => {
+  dispatch('Update not available.')
+})
+
+autoUpdater.on('error', (err) => {
+  dispatch('Error in auto-updater. ' + err)
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  // let log_message = "Download speed: " + progressObj.bytesPerSecond
+  // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  // dispatch(log_message)
+
+    win.webContents.send('download-progress', progressObj.percent)
+
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+  dispatch('Update downloaded')
+})
