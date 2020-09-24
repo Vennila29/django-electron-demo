7544c706a7fe4a54a7d459deb99215335edea9b1
vennila29 <epsivennila@gmail.com>
2020-09-24 15:38:41 +0530

    v2.0.10 commit

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const { app, BrowserWindow } = require('electron')
 const { autoUpdater } = require("electron-updater")
-
+const log = require('electron-log');
+autoUpdater.logger = log;
+log.transports.file.level = 'info';
+log.info('App starting...');
 let win
 
 const dispatch = (data) => {
+  log.info(data);
   win.webContents.send('message', data)
 }
 
 const createDefaultWindow = () => {
-  win = new BrowserWindow()
+  win = new BrowserWindow({ width: 1200, height: 600,webPreferences: {
+    nodeIntegration: true
+  } })
+  win.webContents.openDevTools();
 
   win.on('closed', () => {
     win = null
@@ -27,6 +34,7 @@ app.on('ready', () => {
 
   win.webContents.on('did-finish-load', () => {
     win.webContents.send('version', app.getVersion())
+    console.log("verssion")
   })
 
 })
@@ -53,12 +61,12 @@ autoUpdater.on('error', (err) => {
 })
 
 autoUpdater.on('download-progress', (progressObj) => {
-  // let log_message = "Download speed: " + progressObj.bytesPerSecond
-  // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
-  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
-  // dispatch(log_message)
+  let log_message = "Download speed: " + progressObj.bytesPerSecond
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  dispatch(log_message)
 
-    win.webContents.send('download-progress', progressObj.percent)
+    // win.webContents.send('download-progress', progressObj.percent)
 
 })
 
